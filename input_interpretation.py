93ab935ecf53dacced177c345babad2d6cff85c1
Mohit Lathia <newmohitlathia@gmail.com>
2022-10-31 12:30:50 +0000

    31/10/2022

--- input_interpretation.py
+++ input_interpretation.py
@@ -39,3 +39,46 @@ def checkInput(availableWords,user_input):
                     usedWords.append(aWord)
     
     return usedWords
+
+def cheak_input (avablewords,user_Input):
+    user_Words = user_Input.split(" ")
+    user_Words = []
+    for words in user_Words:
+        if words.lower in avablewords:
+            user_Words.append(words)
+
+
+    if user_Words ==[]:
+        for uWords in avablewords:
+            found_match = False 
+
+            for aWords in range(1,len(uWords)-1):
+                acuarcy =0
+                print(uWords)
+
+            if not found_match:
+
+                for i in range(0,len(uWords)-1):
+
+                    try:
+                        if uWords[i]==aWords[i]:
+                            accuracy+=1
+                    except IndexError:
+                        pass
+
+                    try:
+                        if uWords[i] ==aWords[i]:
+                            accuracy+=1
+                    except IndexError:
+                        pass 
+
+                    try: 
+                        if uWords[i]==aWords[i+1]:
+                            accuracy+=1
+                    except IndexError:
+                            pass
+
+                print(f"final accuracy: {accuracy}")
+                if len(words) - accuracy <=3:
+                    user_Words.append(aWords)
+        return(user_Words)  
